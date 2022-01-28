353efd88763c12230dc56859b60e4851bd46b158
AdamCampeau <adamcampeau3@gmail.com>
2022-01-28 16:02:31 -0500

    find one category by its uid=1053331(adacam) gid=1049089 groups=1049089 value

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -20,16 +20,47 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+  console.log(res)
   // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id:req.params.id,
+    },
   // be sure to include its associated Products
-});
+  include:{
+    model:Product,
+    attributes:["product_name","price","stock"]
+  },
+})
+// used the following as a template
+.then((categoryData) => {
+  if (!categoryData) {
+    res.status(404).json({
+      message: "ERROR 404: PAGE NOT FOUND"
+    })
+    return;
+  }
+})
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+  // template
+  .then((categoryData) => {
+    if (!categoryData) {
+      res.status(404).json({
+        message: "ERROR 404: PAGE NOT FOUND"
+      })
+      return;
+    }
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
