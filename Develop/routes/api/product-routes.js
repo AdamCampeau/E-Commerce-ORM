5aa059cb53284c92af363d1948a5bf46e45b732d
AdamCampeau <adamc@baytechplastics.com>
2022-02-03 13:21:49 -0500

    product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,13 +6,48 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    include: [{
   // be sure to include its associated Category and Tag data
-});
+      model:Category,
+      attributes:['category_name'],
+    },
+    {
+      model: Tag,
+      attributes:['tag_name'],
+    },
+  ]
+})
+  .then((tagData) => res.json(tagData))
+  .catch((err)=> {
+    console.log(err);
+    res.status(400).json(err)
+  })
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne ({
+    where: {
+      id: req.params.id,
+    },
   // be sure to include its associated Category and Tag data
+    include: [
+    {
+      model:Category,
+      attributes:['category_name'],
+    },
+    {
+      model: Tag,
+      attributes:['tag_name'],
+    },
+  ]
+})
+  .then((tagData)=>res.json(tagData))
+  .catch((err)=> {
+    console.log(err)
+  }
+  
 });
 
 // create new product
